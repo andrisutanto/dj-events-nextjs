5312fa319b2867a76aeba19fdb4960775fcee678
Andri Sutanto <me@andrisutanto.com>
2023-01-21 17:23:46 +0800

    Event Details: Page Content

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,54 @@
+import {FaPencilAlt, FaTimes} from 'react-icons/fa'
+import Link from "next/link"
+import Image from "next/image"
 import Layout from "@components/Layout"
 import { API_URL } from "@config/index"
+import styles from "@styles/Event.module.css"
 
 export default function EventPage({evt}) {
+  //dummy event, karena akan error
+  const deleteEvent = (e) => {
+    console.log('delete')
+  }
+
   return (
     <Layout>
-        <h1>{evt.name}</h1>
+        <div className={styles.event}>
+          <div className={styles.controls}>
+            <Link href={`/events/edit/${evt.id}`}>
+              <button className='btn'>
+                <FaPencilAlt />Edit Event
+              </button>
+            </Link>
+            <a href='#' className={styles.delete} onClick={deleteEvent}>
+              <FaTimes /> Delete Event
+            </a>
+          </div>
+
+          <span>
+            {evt.date} at {evt.time}
+          </span>
+          <h1>{evt.name}</h1>
+          {evt.image && (
+            <div className={styles.image}>
+              <Image 
+                src={evt.image}
+                width={960}
+                height={600} />
+            </div>
+          )}
+
+          <h3>Performers:</h3>
+          <p>{evt.performers}</p>
+          <h3>Description:</h3>
+          <p>{evt.description}</p>
+          <h3>Venue: {evt.venue}</h3>
+          <p>{evt.address}</p>
+
+          <Link href='/events'>
+            <button className='btn'>Go Back</button>
+          </Link>
+        </div>
     </Layout>
   )
 }
